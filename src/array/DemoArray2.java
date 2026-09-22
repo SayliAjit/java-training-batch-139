80045cb1b0408c39cefb53cf7005cce1315c4024
sayli <saylibhoge123@gmail.com>
2026-09-22 09:35:21 +0530

    Update demoarray2 program

--- src/array/DemoArray2.java
+++ src/array/DemoArray2.java
@@ -5,8 +5,9 @@ public class DemoArray2 {
 	public static void main(String[] args) {
 		
 		System.out.println("This is Beginning");
+		System.out.println("changes");
 		
-		int[] age = {10,20,30,40,50,60,70};
+		int[] age = {10,20,30,40,50,60,70,44,44,23,64,77,43,90,23,78};
 		
         // 1. length
 		int len = age.length;
